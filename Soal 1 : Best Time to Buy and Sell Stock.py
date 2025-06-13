34def1ab9b3dfaad5bb3591f6e6c3e9f56aa4865
itozt <162090884+itozt@users.noreply.github.com>
2025-06-13 18:27:47 +0700

    Update Soal 1 : Best Time to Buy and Sell Stock.py

--- Soal 1 : Best Time to Buy and Sell Stock.py
+++ Soal 1 : Best Time to Buy and Sell Stock.py
@@ -2,20 +2,15 @@ from typing import List
 
 class Solution: 
     def maxProfit(self, prices: List[int]) -> int:
-        if not prices:  # untuk menangani list kosong
+        if not prices:  
             return 0
 
-        min_price = float('inf')  #inisialisasi min_price dengan nilai tak terbatas
-        max_profit = 0            #inisialisasi max_profit dengan 0
+        min_price = float('inf') 
+        max_profit = 0            
 
-        # melakukan iterasi melalui setiap harga dalam daftar
         for price in prices:
-            # Jika harga saat ini lebih rendah dari min_price yang tercatat,
-            # perbarui min_price. Ini adalah hari terbaik untuk membeli sejauh ini.
             if price < min_price:
                 min_price = price
-            # Jika harga saat ini lebih tinggi dari min_price,
-            # hitung potensi keuntungan dan perbarui max_profit jika lebih besar.
             else:
                 max_profit = max(max_profit, price - min_price)
 
